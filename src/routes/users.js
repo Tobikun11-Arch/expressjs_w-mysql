20f54fbc637fed28798482a6e4843d415e30cdc7
Joenel <joenelsevellejo831@gmail.com>
2026-02-22 21:49:17 +0800

    read users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,24 +1,40 @@
-import express from 'express'
-import pool from '../db.js'
+import express from 'express';
+import pool from '../db.js';
 
-const router = express.Router()
+const router = express.Router();
 
 //#1 CRUD to mysql
 
 //Create
-router.post('/register', async (req,res) => {
+router.post('/register', async (req, res) => {
   const {first_name, last_name, email, password} = req.body;
 
   try {
     const [post_result] = await pool.query(
       'INSERT INTO users (first_name, last_name, email, password) values (?, ?, ?, ?)',
       [first_name, last_name, email, password]
-    )
+    );
 
-    res.json({id: post_result.insertId, first_name, last_name, email, password})
+    res.json({
+      id: post_result.insertId,
+      first_name,
+      last_name,
+      email,
+      password
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({error: error.message});
   }
-})
+});
+
+//Read
+router.get('/view-users', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT * FROM users');
+    res.json({users: rows});
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+});
 
 export default router;
